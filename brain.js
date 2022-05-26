b7997d13036d7ec1ed6478fc59566c6a10bcbc15
unknown <nectariot@gmail.com>
2022-05-26 12:25:54 -0400

    added a topnavigation bar to the home page

--- brain.js
+++ brain.js
@@ -1,12 +1,16 @@
-function createParagraph() {
-    const para = document.createElement('p');
-    para.textContent = 'You clicked the button!';
-    document.body.appendChild(para);
-  }
-  
-  const buttons = document.querySelectorAll('button');
-  
-  for (const button of buttons) {
-    button.addEventListener('click', createParagraph);
-  }
-  
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+canvas.height = canvas.width;
+ctx.transform(1, 0, 0, -1, 0, canvas.height)
+
+const xArray = [50,60,70,80,90,100,110,120,130,140,150];
+const yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+ctx.fillStyle = "red";
+for (let i = 0; i < xArray.length-1; i++) {
+  let x = xArray[i]*400/150;
+  let y = yArray[i]*400/15;
+  ctx.beginPath();
+  ctx.ellipse(x, y, 3, 3, 0, 0, Math.PI * 2);
+  ctx.fill();
+}
